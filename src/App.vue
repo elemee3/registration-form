78f80defe0d35d779c15dc8c5a8b4b35c1046d0a
elemee3 <elemee3@github.com>
2022-05-05 00:52:26 -0700

    basic page and form structure

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,60 @@
-<script setup>
+<script>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  props: {
+    petName: String,
+    petBreed: String,
+    breedType: String
+  },
+  emits: ['update:petName', 'update:petBreed', 'update:breedType']
+}
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="logo" class="logo" src="@/assets/logo.png" width="80" height="80" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+    <div class="menu-items">
+      <a href="#" class="menu-item">Help</a>
+      <a href="#" class="menu-item logo-blue">Save and Finish Later</a>
     </div>
   </header>
 
-  <RouterView />
+  <body>
+    <div class="form">
+      <h1>Tell us about your dog</h1>
+
+      <p class="form-label">What is your dog's name?</p>
+      <input v-model="petName" />
+      <p class="form-label">What breed are they?</p>
+      <select v-model="petBreed">
+        <option disabled value="">Please select one</option>
+        <option>Breed 1</option>
+        <option>Breed 2</option>
+        <option>Breed 3</option>
+        <option>Can't find it?</option>
+      </select>
+
+      <div class="breed-options">
+        <p class="form-label">Choose One</p>
+        <input type="radio" class="breed-option" id="unknown-breed" value="I don't know" v-model="breedType" /><label>I don't know</label><br/>
+        <input type="radio" class="breed-option" id="mix-breed" value="It's a mix" v-model="breedType" /><label>It's a mix</label>
+      </div>
+
+      <p class="form-label">What gender are they?</p>
+      <div class="pillbox-group">
+        <button class="button-style gender-button selected-gender">Female</button>
+        <button class="button-style gender-button">Male</button>
+      </div>
+
+      <div class="continue">
+        <button type="submit" class="button-style submit-button">Continue</button>
+      </div>
+    </div>
+  </body>
+
 </template>
 
 <style>
@@ -26,57 +63,103 @@ import HelloWorld from '@/components/HelloWorld.vue'
 #app {
   max-width: 1280px;
   margin: 0 auto;
-  padding: 2rem;
 
   font-weight: normal;
 }
 
 header {
-  line-height: 1.5;
-  max-height: 100vh;
+  display: flex;
+  justify-content: space-between;
+  background-color: white;
+  padding: 10px;
 }
 
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+  align-items: flex-end;
+  justify-content: flex-end;
 }
 
-a,
-.green {
+.menu-items {
+  display: flex;
+  align-items: flex-end;
+  margin-bottom: 10px;
+}
+
+.menu-item {
+  margin: 5px;
+}
+
+.menu-item:hover {
+  color: #00aeef;
+}
+
+.logo-blue {
+  color: #00aeef;
+}
+
+.logo-blue:hover {
+  color: black;
+}
+
+a {
   text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
   transition: 0.4s;
+  color: black;
 }
 
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
+body {
+  background-color: #f6f6f6;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+.form {
+  background-color: white;
+  width: 70vw;
+  max-width: 500px;
+  min-height: 500px;
+  margin: 50px auto;
+  padding: 35px 55px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.form-label {
+  font-size: 14px;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.breed-options {
+  margin-left: 20px;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+.breed-option {
+  margin: 8px;
 }
 
-nav a:first-of-type {
-  border: 0;
+h1 {
+  color: #2f628f;
+  font-size: 18px;
+}
+
+.button-style {
+  border: 1px solid #00aeef;
+  border-radius: 5px;
+}
+
+.gender-button {
+  background-color: white;
+  color: #00aeef;
+  padding: 5px 25px;
+}
+
+.selected-gender {
+  background-color: #00aeef;
+  color: white;
+}
+
+.submit-button {
+  background-color: #00aeef;
+  color: white;
+  padding: 8px 55px;
 }
 
 @media (min-width: 1024px) {
@@ -103,17 +186,6 @@ nav a:first-of-type {
     flex-wrap: wrap;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
